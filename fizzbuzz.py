cae03739819f1e4e0e59b0cbd150ebd611543af7
Matheus Barbieri <matheus.barbieri@corp.globo.com>
2018-01-09 16:19:59 -0200

    feat: add fizzbuzz algorithm script; close done #2

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+# import sys
+
+
+def fizzbuzz(input_number):
+    output = []
+    for number in range(1, input_number+1):
+        out = ""
+        if not number % 3:
+            out += 'Fizz'
+        if not number % 5:
+            out += 'Buzz'
+        if len(out) != 0:
+            output.append(out)
+        else:
+            output.append(number)
+    return output
