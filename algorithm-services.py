fe121b642a6f07d062f10628b501dc7aade4f00b
Matheus Barbieri <matheus.barbieri@corp.globo.com>
2018-01-23 14:20:12 -0200

    feat: create flask application

--- algorithm-services.py
+++ algorithm-services.py
@@ -1,1 +1,13 @@
-print('algorithm-services')
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Home'
+
+@app.route('/algorithm/<algorithm>')
+def run_algorithm(algorithm):
+    return str(algorithm)
