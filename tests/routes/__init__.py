1c7e77aa851a021801abd3617b8a94fef44cdb74
Matheus Barbieri <matheus.barbieri@corp.globo.com>
2018-01-24 14:00:06 -0200

    fix: remove junk code

--- tests/routes/__init__.py
+++ tests/routes/__init__.py
@@ -1,6 +1,5 @@
-class RouteTestCase(unittest.TestCase):
-
-    def setUp(self):
-        _app = create_app(__name__, {})
-        self.app = _app.test_client()
-        
+# class RouteTestCase(unittest.TestCase):
+#
+#     def setUp(self):
+#         _app = create_app(__name__, {})
+#         self.app = _app.test_client()
