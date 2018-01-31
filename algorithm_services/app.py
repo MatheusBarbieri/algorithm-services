4860d523f425a5d47d2ad52ed96869da261473b9
Matheus Barbieri <matheus.barbieri@corp.globo.com>
2018-01-31 18:31:38 -0200

    feat: add index page information from markdown

--- algorithm_services/app.py
+++ algorithm_services/app.py
@@ -1,20 +1,53 @@
 import json
+import markdown
+import re
+import os.path
+import jinja2
 
 from flask import Flask
 from flask import render_template
 from flask import Response
+from flask import Markup
 
 from algorithm_services.algorithms.fizzbuzz import fizzbuzz
 from algorithm_services.algorithms.clock_angle import clock_angle
+
+
+def get_readme_algorithms_doc(readme_data):
+    found = re.search(
+        '### Available algorithms(.+?)### Tests',
+        readme_data,
+        re.DOTALL
+    )
+    if found:
+        algorithms_data = """### Basic usage
+        - Syntax: <server domain>/<algorithm (lower case)>/<inputs>
+        - Example: localhost:8888/fizzbuzz/20
+        """
+        algorithms_data += '\n### Available Algorithms\n'
+        algorithms_data += found.group(1)
+    return algorithms_data
 
 
 def create_app(name, config):
     app = Flask(name)
     app.config.update(config)
 
+    app_loader = jinja2.FileSystemLoader(
+                os.path.abspath('./algorithm_services/templates') # noqa
+    )
+
+    app.jinja_loader = app_loader
+
     @app.route('/')
     def index():
-        return render_template('index.html')
+        with open(os.path.abspath('README.md')) as file:
+            readme_data = file.read()
+
+        algorithms_data = get_readme_algorithms_doc(readme_data)
+        algorithms = Markup(markdown.markdown(algorithms_data, extensions=['markdown.extensions.tables'])) # noqa
+
+        return render_template('index.html', **locals())
 
     @app.route('/fizzbuzz/<int:number>')
     def fizzbuzz_route(number):
