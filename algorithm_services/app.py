d47c2fcaaee610f84602c87b81766beb0c9f8c19
Matheus Barbieri <matheus.barbieri@corp.globo.com>
2018-02-01 16:50:19 -0200

    refactor: config template folder in flask creation; rename get_index_data function

--- algorithm_services/app.py
+++ algorithm_services/app.py
@@ -2,7 +2,6 @@ import json
 import markdown
 import re
 import os.path
-import jinja2
 
 from flask import Flask
 from flask import render_template
@@ -13,39 +12,34 @@ from algorithm_services.algorithms.fizzbuzz import fizzbuzz
 from algorithm_services.algorithms.clock_angle import clock_angle
 
 
-def get_readme_algorithms_doc(readme_data):
+def get_index_data(readme_data):
     found = re.search(
         '### Available algorithms(.+?)### Tests',
         readme_data,
         re.DOTALL
     )
     if found:
-        algorithms_data = """### Basic usage
-        - Syntax: <server domain>/<algorithm (lower case)>/<inputs>
-        - Example: localhost:8888/fizzbuzz/20
-        """
-        algorithms_data += '\n### Available Algorithms\n'
-        algorithms_data += found.group(1)
-    return algorithms_data
+        index_data = found.group(1)
+    return index_data
 
 
 def create_app(name, config):
-    app = Flask(name)
-    app.config.update(config)
-
-    app_loader = jinja2.FileSystemLoader(
-                os.path.abspath('./algorithm_services/templates') # noqa
-    )
-
-    app.jinja_loader = app_loader
+    template_folder = os.path.abspath(config['TEMPLATE_FOLDER'])
+    app = Flask(name, template_folder=template_folder)
+    app.config.update(config['FLASK_CONFIG'])
 
     @app.route('/')
     def index():
         with open(os.path.abspath('README.md')) as file:
             readme_data = file.read()
 
-        algorithms_data = get_readme_algorithms_doc(readme_data)
-        algorithms = Markup(markdown.markdown(algorithms_data, extensions=['markdown.extensions.tables'])) # noqa
+        index_data = get_index_data(readme_data)
+        algorithms = Markup(
+            markdown.markdown(
+                index_data,
+                extensions=['markdown.extensions.tables']
+                )
+        )
 
         return render_template('index.html', **locals())
 
