7bab6e945fb23932d6e0328735a9c92b7b2f5cef
Matheus Barbieri <matheus.barbieri@corp.globo.com>
2018-02-01 17:40:24 -0200

    tests: add get_index_data test case

--- tests/routes/test_index_route.py
+++ tests/routes/test_index_route.py
@@ -1,12 +1,14 @@
 import unittest
 
 from algorithm_services.app import create_app
+from algorithm_services.app import get_index_data
+from algorithm_services.config import config
 
 
 class IndexRouteTestCase(unittest.TestCase):
 
     def setUp(self):
-        _app = create_app(__name__, {})
+        _app = create_app(__name__, config)
         self.app = _app.test_client()
         self.index = self.app.get('/')
 
@@ -17,3 +19,7 @@ class IndexRouteTestCase(unittest.TestCase):
             b'This is a webapp that permits the execution of algorithms'
             in self.index.data
         )
+
+    def test_get_index_data(self):
+        test_string = '### Available algorithmsABCD### Tests'
+        self.assertEqual(get_index_data(test_string), 'ABCD')
