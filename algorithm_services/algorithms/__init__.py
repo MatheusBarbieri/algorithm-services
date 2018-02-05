5e72f886fcabe56bef1ccbd312d3a6f73b547da0
Matheus Barbieri <matheus.barbieri@corp.globo.com>
2018-02-05 18:57:40 -0200

    feat: create algorithm model

---
+++ algorithm_services/algorithms/__init__.py
@@ -0,0 +1,23 @@
+from flask import abort
+
+
+class Algorithm():
+
+    def __init__(self, args):
+        self.args = args
+
+    def run(self):
+        '''
+            Catchs cases where the user inputs less arguments
+            or arguments with wrong type
+        '''
+        try:
+            result = self.function(*self.args)
+        except TypeError:
+            return abort(404)
+        except ValueError:
+            return abort(404)
+        return result
+
+    def function(self, args):
+        pass
