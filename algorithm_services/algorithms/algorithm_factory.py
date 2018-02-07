36cf7e6d39e1bae390f17bc478828cb843146306
Matheus Barbieri <matheus.barbieri@corp.globo.com>
2018-02-07 15:10:33 -0200

    feat: import modules and decide algorithm dinamically based on name

--- algorithm_services/algorithms/algorithm_factory.py
+++ algorithm_services/algorithms/algorithm_factory.py
@@ -1,12 +1,23 @@
-from algorithm_services.algorithms.fizzbuzz import Fizzbuzz
-from algorithm_services.algorithms.clock_angle import ClockAngle
+import importlib
 
 
 class AlgorithmFactory():
 
     @classmethod
-    def create_algorithm(self, name, args):
-        if name == 'fizzbuzz':
-            return Fizzbuzz(args)
-        if name == 'clock_angle':
-            return ClockAngle(args)
+    def create_algorithm(cls, name, args):
+        module = importlib.import_module(
+            'algorithm_services.algorithms.' + name
+        )
+
+        attr = cls._name_to_class_name(name)
+        Algorithm = getattr(module, attr)
+
+        return Algorithm(args)
+
+    @classmethod
+    def _name_to_class_name(cls, name):
+        out = ""
+        words = name.split('_')
+        for word in words:
+            out += word[0].upper() + word[1:]
+        return out
